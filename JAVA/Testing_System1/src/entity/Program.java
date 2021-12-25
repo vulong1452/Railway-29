b066c21688553e477d66d81019066fa3d0485d7b
vulong1452 <vulong14520@gmail.com>
2021-12-25 19:56:58 +0700

    VuLong
    - xoa project1

--- JAVA/Testing_System1/src/entity/Program.java
+++ JAVA/Testing_System1/src/entity/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		TestingSystem1 testingSystem1 = new TestingSystem1();
-		testingSystem1.Ex1();
+		testingSystem1.Ex1q10();
 
 	}
 
